57a2346af759a3161586a53b8e0e9821df796548
Jared Allard <jaredallard@outlook.com>
2016-10-02 21:12:47 -0700

    ws: remove stale wsproxy ref

--- index.js
+++ index.js
@@ -67,15 +67,7 @@ async.waterfall([
       debug('proxy:err', err);
       res.end('Something went wrong. And we are reporting a custom error message.');
     });
-
-    wsproxy.on('error', function (err, req, res) {
-      res.writeHead(500, {
-        'Content-Type': 'text/plain'
-      });
-
-      debug('wsproxy:err', err);
-      res.end('Something went wrong. And we are reporting a custom error message.');
-    });
+    
     return next();
   },
 
